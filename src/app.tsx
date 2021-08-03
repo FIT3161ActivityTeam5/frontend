bac1a001dfd40c0ab4e39d02b0603fc8c9085821
nomisfailla <35957952+nomisfailla@users.noreply.github.com>
2021-08-04 09:14:58 +1000

    Added tailwind-rn

--- src/app.tsx
+++ src/app.tsx
@@ -1,22 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
+import tailwind from 'tailwind-rn';
 import ExampleComponent from './components/example-component';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tailwind('flex-1 bg-white items-center justify-center')}>
       <ExampleComponent />
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
