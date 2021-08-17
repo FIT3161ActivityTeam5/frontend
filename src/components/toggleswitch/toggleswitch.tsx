460d4ef5fdfba72a744e124fa43ca61e0fadeb5e
Jayden Liddelow <jaydlidd132@gmail.com>
2021-08-18 09:48:53 +1000

    Added comments for readability in toggleswitch.tsx
    Created a new folder and .tsx file for building map containers.

--- src/components/toggleswitch/toggleswitch.tsx
+++ src/components/toggleswitch/toggleswitch.tsx
@@ -1,11 +1,16 @@
 ﻿import React, { useState } from 'react';
 import { View, Switch, StyleSheet, SwitchProps } from 'react-native';
-import tailwind, { getColor } from 'tailwind-rn';
+import { getColor } from 'tailwind-rn';
 
 export default function Toggle_Switch(props: SwitchProps) {
+    // Set two methods to change or update the state of the switch
     const [isEnabled, setIsEnabled] = useState(false);
+
+    // When called, will set state to the opposite of what it was
+    // If on, will turn off and vice versa.
     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
+    // Colour for switch is purple #600 as this is generally fitting for the overall colour scheme being used.
     return (
         <View style={styles.container}>
             <Switch trackColor={{ false: getColor("purple-600"), true: getColor("purple-600") }}
