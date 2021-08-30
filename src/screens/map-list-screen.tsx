135b2c0bc5be83b4a4356c077f1d657f80adb04a
Jayden Liddelow <jaydlidd132@gmail.com>
2021-08-30 17:35:55 +1000

    + Change the location of /assets folder to be within the src folder for easier access from other scripts + Implemented functioning map list view

--- src/screens/map-list-screen.tsx
+++ src/screens/map-list-screen.tsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Button from '../components/button/button';
-import Text from '../components/text/text';
-import ToggleSwitch from '../components/toggleswitch/toggleswitch';
 import useAuthentication from '../hooks/use-authentication';
+import { ListItem, Image } from 'react-native-elements';
+import tailwind from 'tailwind-rn';
 
 export default function MapListScreen() {
-  const auth = useAuthentication();
+    const auth = useAuthentication();
+
+    // Testing list for displaying maps
+    const mapList = [
+        {
+            name: 'Map Number 1',
+            image: require('../assets/adaptive-icon.png')
+        },
+        {
+            name: 'Map Number 2',
+            image: require('../assets/adaptive-icon.png')
+        },
+        {
+            name: 'Map Number 3',
+            image: require('../assets/adaptive-icon.png')
+        }
+    ]
 
   return (
-    <SafeAreaView>
-          <Text>{auth.accessToken}</Text>
-          <ToggleSwitch/>
-      <Button title="Log Out" onPress={auth.logout} />
+      <SafeAreaView style={tailwind('flex-1 bg-white justify-between p-8')}>
+          {
+              mapList.map((l, i) => (
+                  <ListItem key={i} bottomDivider>
+                      <Image source={ l.image } style={{width: 180, height:180}}></Image>
+                      <ListItem.Content>
+                          <ListItem.Title>{l.name}</ListItem.Title>
+                      </ListItem.Content>
+                  </ListItem>
+              ))
+          }
+          <Button title="Log Out" onPress={auth.logout} />
     </SafeAreaView>
   );
 }
