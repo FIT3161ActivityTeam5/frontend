77c7fc535b5c880824388e00443418e4f65509bd
Justin Yeow <jyyeo13@student.monash.edu>
2021-09-01 00:34:05 +1000

    Updated login screen

--- src/screens/login-screen.tsx
+++ src/screens/login-screen.tsx
@@ -3,22 +3,31 @@ import Text from '../components/text/text';
 import Button from '../components/button/button';
 import tailwind from 'tailwind-rn';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { ImageBackground, Image } from 'react-native';
+
 
 /**
  * The LoginScreen contains the login form.
  */
 export default function LoginScreen() {
     return( 
-        
-        <SafeAreaView style={tailwind('flex-1 bg-white items-center justify-between p-8')}>
-
-            <Text style={tailwind('text-purple-800 font-semibold text-3xl')}>
-                ACTIVITY
-            </Text>
-            <Button title="Login" onPress={() => {}} />
-
-        </SafeAreaView>
     
+
+        <SafeAreaView style={tailwind('flex-1 bg-blue-300 items-center justify-around p-8')}>
+           
+            <Text style={tailwind('text-purple-800 font-semibold text-3xl  text-center ')}>
+                Welcome to ACTIVITY
+            </Text>
+
+
+            <Text style={tailwind('text-purple-800 font-semibold text-3xl  text-center')}>
+                An Acceptance and Commitment Therapy Informed Visual Intervention Tool for You
+            </Text>
+
+            <Button title="Continue"  onPress={() => {}} />
+            
+        </SafeAreaView>
+
     )
     ;
 
