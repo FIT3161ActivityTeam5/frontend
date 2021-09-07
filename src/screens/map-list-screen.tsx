6d20b9cac89db71d97e08667e1eb67d7e085d77f
Jayden Liddelow <63001540+jaydlidd@users.noreply.github.com>
2021-09-08 09:53:55 +1000

    Update map-list-screen.tsx

--- src/screens/map-list-screen.tsx
+++ src/screens/map-list-screen.tsx
@@ -1,17 +1,43 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Button from '../components/button/button';
-import Text from '../components/text/text';
-import TextInput from '../components/text_input/input';
 import useAuthentication from '../hooks/use-authentication';
+import { ListItem, Image } from 'react-native-elements';
+import tailwind from 'tailwind-rn';
+import clsx from 'clsx';
 
 export default function MapListScreen() {
-  const auth = useAuthentication();
+    const auth = useAuthentication();
+
+    // Testing list for displaying maps
+    const mapList = [
+        {
+            name: 'Map Number 1',
+            image: require('../assets/adaptive-icon.png')
+        },
+        {
+            name: 'Map Number 2',
+            image: require('../assets/adaptive-icon.png')
+        },
+        {
+            name: 'Map Number 3',
+            image: require('../assets/adaptive-icon.png')
+        }
+    ]
 
   return (
-    <SafeAreaView>
-      {/* <TextInput onChange={() => {}} ></TextInput> */}
-      <Button title="Log Out" onPress={auth.logout} />
+      <SafeAreaView style={tailwind('flex-1 bg-white justify-between p-8')}>
+          {
+              mapList.map((l, i) => (
+                  <ListItem key={i} bottomDivider>
+                      <Image source={ l.image } style={{width: 180, height:180}}></Image>
+                      <ListItem.Content>
+                          <Button title={l.name} onPress={() => console.log("Add")} style={clsx('text-white')}></Button>
+                      </ListItem.Content>
+                  </ListItem>
+              ))
+          }
+          <Button title="Log Out" onPress={auth.logout} />
     </SafeAreaView>
   );
 }
