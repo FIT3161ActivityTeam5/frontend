b3e879b8bd9d1f53bab8a55f25092e453bea5e75
Justin Yeow <jyyeo13@student.monash.edu>
2021-09-08 10:17:02 +1000

    Added tailwind style for toggle switch

--- src/components/toggleswitch/toggleswitch.tsx
+++ src/components/toggleswitch/toggleswitch.tsx
@@ -1,27 +1,28 @@
 ﻿import React, { useState } from 'react';
 import { View, Switch, StyleSheet, SwitchProps } from 'react-native';
 import tailwind, { getColor } from 'tailwind-rn';
+import clsx from 'clsx';
+
 
 export default function Toggle_Switch(props: SwitchProps) {
     const [isEnabled, setIsEnabled] = useState(false);
     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
     return (
-        <View style={styles.container}>
-            <Switch trackColor={{ false: getColor("purple-600"), true: getColor("purple-600") }}
+        <Switch trackColor={{ false: getColor("purple-600"), true: getColor("purple-600") }}
                 thumbColor={isEnabled ? getColor("purple-50") : getColor("purple-50") }
                 ios_backgroundColor = "#3e3e3e"
                 onValueChange = { toggleSwitch }
                 value = { isEnabled }
+                style={tailwind(clsx('text-justify justify-start'))}
         />
-        </View>
   );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+// const styles = StyleSheet.create({
+//     container: {
+//         flex: 1,
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//     },
+// });
--- src/screens/settings-screen.tsx
+++ src/screens/settings-screen.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tailwind from 'tailwind-rn';
 import Text from '../components/text/text';
+import ToggleSwitch from '../components/toggleswitch/toggleswitch';
+
 
 export default function SettingsScreen() {
   return (
     <SafeAreaView style={tailwind("flex-1")}>
       <Text>Hello settings screen</Text>
+      <ToggleSwitch ></ToggleSwitch>
     </SafeAreaView>
   );
 }
