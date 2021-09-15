6aabf7a2769e43d11c457c25b41b5f062d3d2e11
Justin Yeow <jyyeo13@student.monash.edu>
2021-09-15 10:47:44 +1000

    Added new screen, user guide, in progress

--- src/screens/settings-screen.tsx
+++ src/screens/settings-screen.tsx
@@ -5,7 +5,21 @@ import Text from '../components/text/text';
 import ToggleSwitch from '../components/toggleswitch/toggleswitch';
 import Button from '../components/button/button';
 import useAuthentication from '../hooks/use-authentication';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './login-screen';
+import UserGuideScreen from './user-guide-screen';
+import { NavigationContainer } from '@react-navigation/native';
 
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Guide" component={UserGuideScreen} />
+    </Stack.Navigator>
+  );
+}
 
 export default function SettingsScreen() {
   const auth = useAuthentication();
@@ -14,7 +28,7 @@ export default function SettingsScreen() {
     <SafeAreaView style={tailwind("flex-1 p-10")}>
       <Text>Dark Mode</Text>
       <ToggleSwitch></ToggleSwitch>
-      <Button style="mt-2" title="User Guide" onPress={auth.logout} />
+      <Button style="mt-2" title="User Guide" onPress={() => {}} />
       <Button style="mt-2" title="Log Out" onPress={auth.logout} />
     </SafeAreaView>
   );
