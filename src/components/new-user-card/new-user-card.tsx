d3e5fffbea87a0f432832b078451266ca897fdf7
nomisfailla <35957952+nomisfailla@users.noreply.github.com>
2021-09-21 11:29:56 +1000

    Bug fixes

--- src/components/new-user-card/new-user-card.tsx
+++ src/components/new-user-card/new-user-card.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { View } from 'react-native';
-import tailwind from 'tailwind-rn';
-import useCreateMap from '../../hooks/use-create-map';
-import Button from '../button/button';
-import Text from '../text/text';
+import React from "react";
+import { View } from "react-native";
+import tailwind from "tailwind-rn";
+import useCreateMap from "../../hooks/use-create-map";
+import Button from "../button/button";
+import Text from "../text/text";
 
 export type NewUserCardProps = {
   /** Callback to be called after the user has successfully created their first map. */
-  onCreateFirst: () => void;
+  onPressPrompt: () => void;
 };
 
 /**
@@ -16,24 +16,23 @@ export type NewUserCardProps = {
  * a map, or to view the user guide.
  */
 export default function NewUserCard(props: NewUserCardProps) {
-  const [createMap, loading] = useCreateMap();
-
   return (
-    <View style={tailwind('items-center p-8')}>
-      <Text style={tailwind('text-xl text-center text-gray-600')}>
-        Looks like you don't have any maps, press the button below to get started.
+    <View style={tailwind("items-center p-8")}>
+      <Text style={tailwind("text-xl text-center text-gray-600")}>
+        Looks like you don't have any maps, press the button below to get
+        started.
       </Text>
       <Button
         title={"New Map"}
-        loading={loading}
-        onPress={() => createMap(() => {
-          props.onCreateFirst();
-        })}
+        onPress={() => {
+          props.onPressPrompt();
+        }}
         style="mt-8 h-10 w-2/3"
       />
-      <Text style={tailwind('text-sm text-center text-gray-600 mt-2')}>
-        Unsure where to begin?{'\n'}
-        Check out the user guide in the <Text style={tailwind("text-purple-700")}>settings screen</Text>.
+      <Text style={tailwind("text-sm text-center text-gray-600 mt-2")}>
+        Unsure where to begin?{"\n"}
+        Check out the user guide in the{" "}
+        <Text style={tailwind("text-purple-700")}>settings screen</Text>.
       </Text>
     </View>
   );
